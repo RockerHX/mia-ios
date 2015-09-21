70916b24644874529bee285ee05826bd27e98d55
RockerHX <RockerHX@gmail.com>
2015-09-21 17:20:39 +0800

    登录界面UI

--- mia/UI/Login/LoginViewController.m
+++ mia/UI/Login/LoginViewController.m
@@ -21,7 +21,7 @@ static const CGFloat kGuidButtonMarginLeft		= 30;
 static const CGFloat kSignInMarginBottom		= 50;
 static const CGFloat kSignUpMarginBottom		= kSignInMarginBottom + kGuidButtonHeight + 15;
 
-@interface LoginViewController ()
+@interface LoginViewController () <UITextFieldDelegate>
 
 @end
 
@@ -31,6 +31,8 @@ static const CGFloat kSignUpMarginBottom		= kSignInMarginBottom + kGuidButtonHei
 	UIView *guidView;		// 注册或者登录两个按钮
 
 	UIView *loginView;		// 输入框和登录按钮的页面
+	UITextField *userNameTextField;
+	UITextField *passwordTextField;
 }
 
 - (void)viewDidLoad {
@@ -85,6 +87,7 @@ static const CGFloat kSignUpMarginBottom		= kSignInMarginBottom + kGuidButtonHei
 	[self.view addSubview:bgImageView];
 
 	[self initGuidView];
+	[self initLoginView];
 
 	UIImage *backButtonImage = [UIImage imageNamed:@"back"];
 	CGRect backButtonFrame = {.origin.x = kBackButtonMarginLeft,
@@ -140,6 +143,109 @@ static const CGFloat kSignUpMarginBottom		= kSignInMarginBottom + kGuidButtonHei
 	[guidView addSubview:signInButton];
 }
 
+- (void)initLoginView {
+	loginView = [[UIView alloc] initWithFrame:self.view.bounds];
+	//loginView.backgroundColor = [UIColor yellowColor];
+	loginView.hidden = YES;
+	[self.view addSubview:loginView];
+
+	static const CGFloat kLoginButtonMarginLeft		= 30;
+	static const CGFloat kTextEditHeight			= 40;
+	static const CGFloat kUserNameMarginTop			= 110;
+	static const CGFloat kPasswordMarginTop			= kUserNameMarginTop + kTextEditHeight + 5;
+	static const CGFloat kForgotPwdMarginTop		= kPasswordMarginTop + kTextEditHeight + 10;
+	static const CGFloat kForgotPwdMarginRight		= kLoginButtonMarginLeft;
+	static const CGFloat kForgotPwdWidth			= 50;
+	static const CGFloat kForgotPwdHeight			= 20;
+	static const CGFloat kLoginMarginTop			= kPasswordMarginTop + kTextEditHeight + 45;
+
+	userNameTextField = [[UITextField alloc] initWithFrame:CGRectMake(kLoginButtonMarginLeft,
+																	  kUserNameMarginTop,
+																	  loginView.frame.size.width - 2 * kLoginButtonMarginLeft,
+																	  kTextEditHeight)];
+	userNameTextField.borderStyle = UITextBorderStyleNone;
+	userNameTextField.backgroundColor = [UIColor clearColor];
+	userNameTextField.textColor = [UIColor whiteColor];
+	userNameTextField.placeholder = @"输入手机号";
+	[userNameTextField setFont:UIFontFromSize(16)];
+	userNameTextField.keyboardType = UIKeyboardTypeNumberPad;
+	userNameTextField.returnKeyType = UIReturnKeyNext;
+	userNameTextField.delegate = self;
+	[userNameTextField setValue:[UIColor whiteColor] forKeyPath:@"_placeholderLabel.textColor"];
+	[loginView addSubview:userNameTextField];
+
+	UIView *lineView1 = [[UIView alloc] initWithFrame:CGRectMake(kLoginButtonMarginLeft,
+																	   kUserNameMarginTop + kTextEditHeight,
+																	   loginView.frame.size.width - 2 * kLoginButtonMarginLeft,
+																	   0.5)];
+	lineView1.backgroundColor = [UIColor grayColor];
+	[loginView addSubview:lineView1];
+
+	passwordTextField = [[UITextField alloc] initWithFrame:CGRectMake(kLoginButtonMarginLeft,
+																	  kPasswordMarginTop,
+																	  loginView.frame.size.width - 2 * kLoginButtonMarginLeft,
+																	  kTextEditHeight)];
+	passwordTextField.borderStyle = UITextBorderStyleNone;
+	passwordTextField.backgroundColor = [UIColor clearColor];
+	passwordTextField.textColor = [UIColor whiteColor];
+	passwordTextField.placeholder = @"密码";
+	passwordTextField.secureTextEntry = YES;
+	[passwordTextField setFont:UIFontFromSize(16)];
+	passwordTextField.keyboardType = UIKeyboardTypeDefault;
+	passwordTextField.returnKeyType = UIReturnKeyDone;
+	passwordTextField.delegate = self;
+	[passwordTextField setValue:[UIColor whiteColor] forKeyPath:@"_placeholderLabel.textColor"];
+	[loginView addSubview:passwordTextField];
+
+	UIView *lineView2 = [[UIView alloc] initWithFrame:CGRectMake(kLoginButtonMarginLeft,
+																 kPasswordMarginTop + kTextEditHeight,
+																 loginView.frame.size.width - 2 * kLoginButtonMarginLeft,
+																 0.5)];
+	lineView2.backgroundColor = [UIColor grayColor];
+	[loginView addSubview:lineView2];
+
+	CGRect forgotPwdButtonFrame = {.origin.x = loginView.frame.size.width - kForgotPwdMarginRight - kForgotPwdWidth,
+		.origin.y = kForgotPwdMarginTop,
+		.size.width = kForgotPwdWidth,
+		.size.height = kForgotPwdHeight};
+	MIAButton *forgotPwdButton = [[MIAButton alloc] initWithFrame:forgotPwdButtonFrame
+												  titleString:@"忘记密码"
+												   titleColor:[UIColor whiteColor]
+														 font:UIFontFromSize(12)
+													  logoImg:nil
+											  backgroundImage:nil];
+	[forgotPwdButton addTarget:self action:@selector(forgotPwdButtonAction:) forControlEvents:UIControlEventTouchUpInside];
+	[loginView addSubview:forgotPwdButton];
+
+
+	CGRect loginButtonFrame = {.origin.x = kLoginButtonMarginLeft,
+		.origin.y = kLoginMarginTop,
+		.size.width = loginView.frame.size.width - 2 * kLoginButtonMarginLeft,
+		.size.height = kGuidButtonHeight};
+	MIAButton *loginButton = [[MIAButton alloc] initWithFrame:loginButtonFrame
+												   titleString:@"登录"
+													titleColor:[UIColor whiteColor]
+														  font:UIFontFromSize(16)
+													   logoImg:nil
+											   backgroundImage:[UIImage imageExtrude:[UIImage imageNamed:@"button_blue"]]];
+	[loginButton addTarget:self action:@selector(loginButtonAction:) forControlEvents:UIControlEventTouchUpInside];
+	[loginView addSubview:loginButton];
+}
+
+#pragma mark -delegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+	if (textField == userNameTextField) {
+		[passwordTextField becomeFirstResponder];
+	}
+	else if (textField == passwordTextField) {
+		[passwordTextField resignFirstResponder];
+	}
+	
+	return true;
+}
+
 #pragma mark - Actions
 
 - (void)backButtonAction:(id)sender {
@@ -150,6 +256,15 @@ static const CGFloat kSignUpMarginBottom		= kSignInMarginBottom + kGuidButtonHei
 }
 
 - (void)signInButtonAction:(id)sender {
+	[guidView setHidden:YES];
+	[loginView setHidden:NO];
+}
+
+
+- (void)forgotPwdButtonAction:(id)sender {
+}
+
+- (void)loginButtonAction:(id)sender {
 }
 
 @end
