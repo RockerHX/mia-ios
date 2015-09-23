49b82f3fa00c9254fe22277b4527c9d87fb5ff09
RockerHX <RockerHX@gmail.com>
2015-09-23 17:38:00 +0800

    收藏Bar的界面初始化实现

--- mia/UI/Profile/ProfileViewController.m
+++ mia/UI/Profile/ProfileViewController.m
@@ -8,13 +8,16 @@
 
 #import "ProfileViewController.h"
 #import "MIAButton.h"
-//#import "ProfileTableView.h"
+#import "MIALabel.h"
+#import "UIImageView+WebCache.h"
+#import "UIImageView+BlurredImage.h"
 #import "ProfileCollectionViewCell.h"
 
 static NSString * const kProfileCellReuseIdentifier = @"ProfileCellId";
 static NSString * const kProfileHeaderReuseIdentifier = @"ProfileHeaderId";
 static const CGFloat kProfileItemMarginH = 10;
 static const CGFloat kProfileItemMarginV = 10;
+static const CGFloat kProfileHeight = 240;
 
 @interface ProfileViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
 
@@ -77,8 +80,6 @@ static const CGFloat kProfileItemMarginV = 10;
 //	ProfileTableView *tableView = [[ProfileTableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
 //	[self.view addSubview:tableView];
 
-	static const CGFloat kProfileHeight = 200;
-
 	//1.初始化layout
 	UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
 	//设置collectionView滚动方向
@@ -128,6 +129,160 @@ static const CGFloat kProfileItemMarginV = 10;
 	UIBarButtonItem *rightButton = [[UIBarButtonItem alloc] initWithCustomView:settingButton];
 	self.navigationItem.rightBarButtonItem = rightButton;
 	[settingButton addTarget:self action:@selector(settingButtonAction:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)initHeaderView:(UIView *)headerView {
+	//headerView.backgroundColor = [UIColor brownColor];
+
+	static const CGFloat kCoverMarginTop = 44;
+	static const CGFloat kCoverHeight = kProfileHeight - kCoverMarginTop * 2;
+	static const CGFloat kPlayButtonMarginRight = 76;
+	static const CGFloat kPlayButtonMarginTop = 109;
+	static const CGFloat kPlayButtonWidth = 40;
+
+	CGRect coverFrame = CGRectMake(0,
+								   kCoverMarginTop,
+								   headerView.frame.size.width,
+								   kCoverHeight);
+	UIImageView *coverImageView = [[UIImageView alloc] initWithFrame:coverFrame];
+	//[coverImageView setImage:[UIImage imageNamed:@"default_cover"]];
+	[coverImageView setImageToBlur:[UIImage imageNamed:@"default_cover"] blurRadius:6.0 completionBlock:nil];
+	[headerView addSubview:coverImageView];
+	UIImageView *coverMaskImageView = [[UIImageView alloc] initWithFrame:coverFrame];
+	[coverMaskImageView setImage:[UIImage imageNamed:@"cover_mask"]];
+	[headerView addSubview:coverMaskImageView];
+
+	MIAButton *playButton = [[MIAButton alloc] initWithFrame:CGRectMake(headerView.frame.size.width - kPlayButtonMarginRight - kPlayButtonWidth,
+															 kPlayButtonMarginTop,
+															 kPlayButtonWidth,
+															 kPlayButtonWidth)
+									  titleString:nil
+									   titleColor:nil
+											 font:nil
+										  logoImg:nil
+								  backgroundImage:nil];
+	[playButton setBackgroundImage:[UIImage imageNamed:@"play"] forState:UIControlStateNormal];
+	[playButton addTarget:self action:@selector(playButtonAction:) forControlEvents:UIControlEventTouchUpInside];
+	[headerView addSubview:playButton];
+
+	const static CGFloat kFavoriteCountLabelMarginRight		= 220;
+	const static CGFloat kFavoriteCountLabelMarginTop		= 108;
+	const static CGFloat kFavoriteCountLabelHeight			= 38;
+
+	static const CGFloat kFavoriteMiddleLabelMarginRight 	= 120;
+	static const CGFloat kFavoriteMiddleLabelMarginTop 		= 108;
+	static const CGFloat kFavoriteMiddleLabelWidth 			= 100;
+	static const CGFloat kFavoriteMiddleLabelHeight 		= 20;
+
+	static const CGFloat kCachedCountLabelMarginRight 		= 120;
+	static const CGFloat kCachedCountLabelMarginTop 		= 130;
+	static const CGFloat kCachedCountLabelWidth 			= 100;
+	static const CGFloat kCachedCountLabelHeight 			= 20;
+
+	MIALabel *favoriteCountLabel = [[MIALabel alloc] initWithFrame:CGRectMake(0,
+																		 kFavoriteCountLabelMarginTop,
+																		 headerView.frame.size.width - kFavoriteCountLabelMarginRight,
+																		 kFavoriteCountLabelHeight)
+														 text:@"30"
+														 font:UIFontFromSize(35.0f)
+														 textColor:[UIColor whiteColor]
+									   textAlignment:NSTextAlignmentRight
+												  numberLines:1];
+	//favoriteCountLabel.backgroundColor = [UIColor blueColor];
+	[headerView addSubview:favoriteCountLabel];
+
+	MIALabel *favoriteMiddleLabel = [[MIALabel alloc] initWithFrame:CGRectMake(headerView.frame.size.width - kFavoriteMiddleLabelMarginRight - kFavoriteMiddleLabelWidth,
+																			  kFavoriteMiddleLabelMarginTop,
+																			  kFavoriteMiddleLabelWidth,
+																			  kFavoriteMiddleLabelHeight)
+															  text:@"首收藏歌曲》"
+															  font:UIFontFromSize(16.0f)
+														 textColor:[UIColor whiteColor]
+													 textAlignment:NSTextAlignmentRight
+													   numberLines:1];
+	//favoriteMiddleLabel.backgroundColor = [UIColor greenColor];
+	[headerView addSubview:favoriteMiddleLabel];
+
+	MIALabel *cachedCountLabel = [[MIALabel alloc] initWithFrame:CGRectMake(headerView.frame.size.width - kCachedCountLabelMarginRight - kCachedCountLabelWidth,
+																			   kCachedCountLabelMarginTop,
+																			   kCachedCountLabelWidth,
+																			   kCachedCountLabelHeight)
+															   text:@"28首已下载到本地"
+															   font:UIFontFromSize(12.0f)
+														  textColor:[UIColor whiteColor]
+													  textAlignment:NSTextAlignmentLeft
+														numberLines:1];
+	//cachedCountLabel.backgroundColor = [UIColor greenColor];
+	[headerView addSubview:cachedCountLabel];
+
+	const static CGFloat kFavoriteIconMarginLeft	= 15;
+	const static CGFloat kFavoriteIconMarginTop		= 15;
+	const static CGFloat kFavoriteIconMarginWidth	= 16;
+
+	const static CGFloat kFavoriteLabelMarginLeft	= kFavoriteIconMarginLeft + kFavoriteIconMarginWidth + 8;
+	const static CGFloat kFavoriteLabelMarginTop	= 13;
+	const static CGFloat kFavoriteLabelWidth		= 30;
+	const static CGFloat kFavoriteLabelHeight		= 20;
+
+	const static CGFloat kShareIconMarginLeft		= 15;
+	const static CGFloat kShareIconMarginTop		= 15 + kCoverHeight + kCoverMarginTop;;
+	const static CGFloat kShareIconMarginWidth		= 16;
+
+	const static CGFloat kShareLabelMarginLeft		= kShareIconMarginLeft + kShareIconMarginWidth + 8;
+	const static CGFloat kShareLabelMarginTop		= 13 + kCoverHeight + kCoverMarginTop;
+	const static CGFloat kShareLabelWidth			= 30;
+	const static CGFloat kShareLabelHeight			= 20;
+
+
+	// 两个子标题
+	UIImageView *favoriteIconImageView = [[UIImageView alloc] initWithFrame:CGRectMake(kFavoriteIconMarginLeft,
+																				kFavoriteIconMarginTop,
+																				kFavoriteIconMarginWidth,
+																				kFavoriteIconMarginWidth)];
+	[favoriteIconImageView setImage:[UIImage imageNamed:@"favorite_white"]];
+	[headerView addSubview:favoriteIconImageView];
+	MIALabel *favoriteLabel = [[MIALabel alloc] initWithFrame:CGRectMake(kFavoriteLabelMarginLeft,
+														  kFavoriteLabelMarginTop,
+														  kFavoriteLabelWidth,
+														  kFavoriteLabelHeight)
+										  text:@"收藏"
+										  font:UIFontFromSize(12.0f)
+										   textColor:UIColorFromHex(@"a2a2a2", 1.0)
+									   textAlignment:NSTextAlignmentLeft
+								   numberLines:1];
+	[headerView addSubview:favoriteLabel];
+
+	UIImageView *shareIconImageView = [[UIImageView alloc] initWithFrame:CGRectMake(kShareIconMarginLeft,
+																					   kShareIconMarginTop,
+																					   kShareIconMarginWidth,
+																					   kShareIconMarginWidth)];
+	[shareIconImageView setImage:[UIImage imageNamed:@"share"]];
+	[headerView addSubview:shareIconImageView];
+	MIALabel *shareLabel = [[MIALabel alloc] initWithFrame:CGRectMake(kShareLabelMarginLeft,
+																		 kShareLabelMarginTop,
+																		 kShareLabelWidth,
+																		 kShareLabelHeight)
+														 text:@"分享"
+														 font:UIFontFromSize(12.0f)
+										   textColor:UIColorFromHex(@"a2a2a2", 1.0)
+									   textAlignment:NSTextAlignmentLeft
+												  numberLines:1];
+	[headerView addSubview:shareLabel];
+
+	static const CGFloat kWifiTipsLabelMarginTop 	= kCoverHeight + kCoverMarginTop - 25;
+	static const CGFloat kWifiTipsLabelHeight		= 20;
+
+	MIALabel *wifiTipsLabel = [[MIALabel alloc] initWithFrame:CGRectMake(0,
+																		 kWifiTipsLabelMarginTop,
+																		 headerView.frame.size.width,
+																		 kWifiTipsLabelHeight)
+														 text:@"在非WIFI网络下，播放收藏歌曲不产生任何流量"
+														 font:UIFontFromSize(12.0f)
+										   textColor:[UIColor whiteColor]
+									   textAlignment:NSTextAlignmentCenter
+												  numberLines:1];
+	[headerView addSubview:wifiTipsLabel];
+
 }
 
 #pragma mark - delegate
@@ -191,11 +346,9 @@ static const CGFloat kProfileItemMarginV = 10;
 //通过设置SupplementaryViewOfKind 来设置头部或者底部的view，其中 ReuseIdentifier 的值必须和 注册是填写的一致，本例都为 “reusableView”
 - (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
 	UICollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:kProfileHeaderReuseIdentifier forIndexPath:indexPath];
-	headerView.backgroundColor =[UIColor grayColor];
-	UILabel *label = [[UILabel alloc] initWithFrame:headerView.bounds];
-	label.text = @"这是collectionView的头部";
-	label.font = [UIFont systemFontOfSize:20];
-	[headerView addSubview:label];
+
+	[self initHeaderView:headerView];
+
 	return headerView;
 }
 
@@ -218,6 +371,10 @@ static const CGFloat kProfileItemMarginV = 10;
 
 - (void)settingButtonAction:(id)sender {
 	NSLog(@"setting button clicked.");
+}
+
+- (void)playButtonAction:(id)sender {
+	NSLog(@"play button clicked.");
 }
 
 @end
