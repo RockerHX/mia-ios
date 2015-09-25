db763b1131a6b7b3fd72fab6c3f9846dc4c4a6b1
RockerHX <RockerHX@gmail.com>
2015-09-25 15:56:47 +0800

    remove code auto generated by core data

--- mia/AppDelegate.h
+++ mia/AppDelegate.h
@@ -16,13 +16,5 @@
 @property (retain, nonatomic) UINavigationController *navigationController;
 @property (strong, nonatomic) RadioViewController *radioViewController;
 
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
-
-
 @end
 
