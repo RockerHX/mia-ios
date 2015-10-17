880318cb43ca70ef462cbc0ae0757ad7d06c6bad
RockerHX <RockerHX@gmail.com>
2015-10-17 15:51:12 +0800

    播放下一首 的通知

--- mia/Utils/MusicPlayerMgr/MusicPlayerMgr.h
+++ mia/Utils/MusicPlayerMgr/MusicPlayerMgr.h
@@ -22,6 +22,12 @@ extern NSString * const MusicPlayerMgrNotificationCompletion;
  */
 +(id)standard;
 
+// 不同模块都可以调用播放器，需要记录当前使用播放器的是哪个模块
+// 使用模块的实例对象地址来做ModelID
+// 如果用枚举值的话很容易忘记修改
+// linyehui
+@property (assign, nonatomic) long currentModelID;
+
 - (BOOL)isPlaying;
 - (void)playWithModelID:(long)modelID url:(NSString*)url title:(NSString *)title artist:(NSString *)artist;
 - (void)pause;
--- mia/Utils/MusicPlayerMgr/MusicPlayerMgr.m
+++ mia/Utils/MusicPlayerMgr/MusicPlayerMgr.m
@@ -30,12 +30,6 @@ NSString * const MusicPlayerMgrNotificationCompletion			= @"MusicPlayerMgrNotifi
 
 @implementation MusicPlayerMgr {
 	FSAudioStream 	*audioStream;
-
-	// 不同模块都可以调用播放器，需要记录当前使用播放器的是哪个模块
-	// 使用模块的实例对象地址来做ModelID
-	// 如果用枚举值的话很容易忘记修改
-	// linyehui
-	long			_currentModelID;
 }
 
 /**
@@ -61,9 +55,9 @@ NSString * const MusicPlayerMgrNotificationCompletion			= @"MusicPlayerMgrNotifi
 		audioStream.strictContentTypeChecking = NO;
 		audioStream.defaultContentType = @"audio/mpeg";
 
-		NSDictionary *userInfo = [NSDictionary dictionaryWithObject:[NSNumber numberWithLong:_currentModelID]
-															 forKey:MusicPlayerMgrNotificationKey_ModelID];
 		audioStream.onCompletion = ^() {
+			NSDictionary *userInfo = [NSDictionary dictionaryWithObject:[NSNumber numberWithLong:[[MusicPlayerMgr standard] currentModelID]]
+																 forKey:MusicPlayerMgrNotificationKey_ModelID];
 			[[NSNotificationCenter defaultCenter] postNotificationName:MusicPlayerMgrNotificationCompletion
 																object:nil
 															  userInfo:userInfo];
