507dca552b38f9a89fd4a0f56fef16552691eb12
xiaohua <xiaohua@miamusic.com>
2015-10-20 18:07:36 +0800

    更新：
    1. 为HXRadioView加载提供异常捕获处理，减少crash

---
+++ mia/Views/HomePage/HXInfectUserView.h
@@ -0,0 +1,13 @@
+//
+//  HXInfectUserView.h
+//  mia
+//
+//  Created by miaios on 15/10/20.
+//  Copyright © 2015年 Mia Music. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXInfectUserView : UIView
+
+@end
