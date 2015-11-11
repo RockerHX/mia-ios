d09228df25c722a751794dd410cbe83ec412c6d6
RockerHX <RockerHX@gmail.com>
2015-11-11 16:11:37 +0800

    接入Testin崩溃SDK

--- mia/Macros/HXAppConstants.m
+++ mia/Macros/HXAppConstants.m
@@ -9,6 +9,7 @@
 
 #pragma mark - Third SDK Key
 NSString *const UMengAPPKEY                                = @"562730aee0f55ab9370019d8";
+NSString *const TestinAPPKEY                               = @"341b2ac47c99919526cf833d6957a08b";
 NSString *const BaiDuMapKEY                                = @"wNZGKBVyuCobA8GcksWWx9xD";
 NSString *const WeiXinKEY                                  = @"wx6194bafa9ae065cc";
 
@@ -19,3 +20,7 @@ NSString *const HXWeiXinPayFailureNotification             = @"HXWeiXinPayFailur
 NSString *const HXApplicationDidBecomeActiveNotification            = @"HXApplicationDidBecomeActiveNotification";
 NSString *const HXMusicPlayerMgrDidPlayNotification                 = @"HXMusicPlayerMgrDidPlayNotification";
 NSString *const HXMusicPlayerMgrDidPauseNotification                = @"HXMusicPlayerMgrDidPauseNotification";
+
+#pragma mark - App Constants
+NSString *const AppstoreChannel     = @"App Store";
+NSString *const FirimChannel        = @"Fir.im";
