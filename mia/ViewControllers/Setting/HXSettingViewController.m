5ef029596092538baeb99174c28244ea4f8f5166
RockerHX <RockerHX@gmail.com>
2015-11-20 17:30:09 +0800

    Update

---
+++ mia/ViewControllers/Setting/HXSettingViewController.m
@@ -0,0 +1,22 @@
+//
+//  HXSettingViewController.m
+//  mia
+//
+//  Created by miaios on 15/11/20.
+//  Copyright © 2015年 Mia Music. All rights reserved.
+//
+
+#import "HXSettingViewController.h"
+
+@interface HXSettingViewController ()
+
+@end
+
+@implementation HXSettingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
