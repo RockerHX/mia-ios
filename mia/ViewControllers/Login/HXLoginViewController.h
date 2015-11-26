4d25c00354f2266f1a74e10b64a6c94991f1f555
RockerHX <RockerHX@gmail.com>
2015-11-26 11:39:55 +0800

    更新：
    1.目录结构
    2.增加新版登录模块

---
+++ mia/ViewControllers/Login/HXLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  HXLoginViewController.h
+//  mia
+//
+//  Created by miaios on 15/11/26.
+//  Copyright © 2015年 Mia Music. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXLoginViewController : UIViewController
+
+@end
