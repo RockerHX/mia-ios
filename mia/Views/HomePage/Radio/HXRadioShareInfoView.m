d2b6de1d2badeee2bc6c93b76fa69e3016ce949f
RockerHX <RockerHX@gmail.com>
2016-01-26 10:48:38 +0800

    Update

---
+++ mia/Views/HomePage/Radio/HXRadioShareInfoView.m
@@ -0,0 +1,16 @@
+//
+//  HXRadioShareInfoView.m
+//  mia
+//
+//  Created by miaios on 16/1/26.
+//  Copyright © 2016年 Mia Music. All rights reserved.
+//
+
+#import "HXRadioShareInfoView.h"
+#import "HXXib.h"
+
+@implementation HXRadioShareInfoView
+
+HXXibImplementation
+
+@end
