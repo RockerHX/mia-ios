cb467335a97da011955108b46f7f0a9a2106762a
RockerHX <RockerHX@gmail.com>
2016-01-28 12:29:25 +0800

    Update

--- mia/ViewControllers/Login/HXLoginViewController.m
+++ mia/ViewControllers/Login/HXLoginViewController.m
@@ -260,7 +260,7 @@ typedef NS_ENUM(BOOL, HXLoginAction) {
      ^(MiaRequestItem *requestItem, BOOL success, NSDictionary *userInfo) {
          __strong __typeof__(self)strongSelf = weakSelf;
          if (success) {
-             [[UserSession standard] setUid:userInfo[MiaAPIKey_Values][@"uid"]];
+             [[UserSession standard] setUid:[userInfo[MiaAPIKey_Values][@"uid"] stringValue]];
              [[UserSession standard] setNick:userInfo[MiaAPIKey_Values][@"nick"]];
              [[UserSession standard] setUtype:userInfo[MiaAPIKey_Values][@"utype"]];
              [[UserSession standard] setUnreadCommCnt:[userInfo[MiaAPIKey_Values][@"unreadCommCnt"] intValue]];
@@ -301,7 +301,7 @@ typedef NS_ENUM(BOOL, HXLoginAction) {
      ^(MiaRequestItem *requestItem, BOOL success, NSDictionary *userInfo) {
          __strong __typeof__(self)strongSelf = weakSelf;
          if (success) {
-             [[UserSession standard] setUid:userInfo[MiaAPIKey_Values][@"uid"]];
+             [[UserSession standard] setUid:[userInfo[MiaAPIKey_Values][@"uid"] stringValue]];
              [[UserSession standard] setNick:userInfo[MiaAPIKey_Values][@"nick"]];
              [[UserSession standard] setUtype:userInfo[MiaAPIKey_Values][@"utype"]];
              [[UserSession standard] setUnreadCommCnt:[userInfo[MiaAPIKey_Values][@"unreadCommCnt"] intValue]];
