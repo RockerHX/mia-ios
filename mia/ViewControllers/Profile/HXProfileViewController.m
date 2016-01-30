2f1b2c092168e74d824db64fe6a8130ae63c3224
RockerHX <RockerHX@gmail.com>
2016-01-30 11:30:53 +0800

    个人页的导航栏状态问题，先把push变量去掉了

--- mia/ViewControllers/Profile/HXProfileViewController.m
+++ mia/ViewControllers/Profile/HXProfileViewController.m
@@ -21,7 +21,7 @@ HXProfileDetailContainerViewControllerDelegate
 @end
 
 @implementation HXProfileViewController {
-    BOOL  _pushed;
+//    BOOL  _pushed;
     UIStatusBarStyle  _statusBarStyle;
     HXProfileCoverContainerViewController *_coverContainerViewController;
     HXProfileDetailContainerViewController *_detailContainerViewController;
@@ -31,18 +31,16 @@ HXProfileDetailContainerViewControllerDelegate
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
-    [self.navigationController setNavigationBarHidden:YES animated:_pushed];
-    if (_pushed) {
-        _pushed = NO;
-    }
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+//	_pushed = NO;
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     
-    if (!_pushed) {
-        return;
-    }
+//    if (!_pushed && _type) {
+//        return;
+//    }
     [self.navigationController setNavigationBarHidden:NO animated:YES];
 }
 
@@ -90,7 +88,7 @@ HXProfileDetailContainerViewControllerDelegate
 
 #pragma mark - Event Response
 - (IBAction)settingButtonPressed {
-    _pushed = YES;
+//    _pushed = YES;
     HXSettingViewController *settingViewController = [HXSettingViewController instance];
     [self.navigationController pushViewController:settingViewController animated:YES];
 }
