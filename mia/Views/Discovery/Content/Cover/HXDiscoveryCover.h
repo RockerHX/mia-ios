cf6a7914f951450774a1662de225bf23f072a059
RockerHX <RockerHX@gmail.com>
2016-02-22 11:15:19 +0800

    完善【发现】封面UI元素基本布局

---
+++ mia/Views/Discovery/Content/Cover/HXDiscoveryCover.h
@@ -0,0 +1,13 @@
+//
+//  HXDiscoveryCover.h
+//  mia
+//
+//  Created by miaios on 16/2/22.
+//  Copyright © 2016年 Mia Music. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXDiscoveryCover : UIView
+
+@end
---
+++ mia/Views/Discovery/Content/Cover/HXDiscoveryCover.m
@@ -0,0 +1,16 @@
+//
+//  HXDiscoveryCover.m
+//  mia
+//
+//  Created by miaios on 16/2/22.
+//  Copyright © 2016年 Mia Music. All rights reserved.
+//
+
+#import "HXDiscoveryCover.h"
+#import "HXXib.h"
+
+@implementation HXDiscoveryCover
+
+HXXibImplementation
+
+@end
