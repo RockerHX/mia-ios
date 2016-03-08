77d2a499240314561f8ca0e7075c248e2cd08cd8
RockerHX <RockerHX@gmail.com>
2016-03-08 16:22:50 +0800

    新增：引导页面

---
+++ mia/Views/Guide/HXGuideView.m
@@ -0,0 +1,96 @@
+//
+//  HXGuideView.m
+//  mia
+//
+//  Created by miaios on 15/10/21.
+//  Copyright © 2015年 Mia Music. All rights reserved.
+//
+
+#import "HXGuideView.h"
+#import "AppDelegate.h"
+
+NSString *const kGuideViewShowKey = @"kGuideViewShowKey";
+
+typedef void(^BLOCK)(void);
+
+@implementation HXGuideView {
+    BLOCK _finishedBlock;
+}
+
+#pragma mark - Class Methods
++ (instancetype)showGuide:(void(^)(void))finished {
+    HXGuideView *guideView = [[[NSBundle mainBundle] loadNibNamed:@"HXGuideView" owner:self options:nil] firstObject];
+    [guideView showGuide:finished];
+    return guideView;
+}
+
++ (BOOL)shouldShow {
+    NSNumber *showed = [[NSUserDefaults standardUserDefaults] valueForKey:kGuideViewShowKey];
+    return !showed.boolValue;
+}
+
+#pragma mark - Init Methods
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+    [self initConfig];
+    [self viewConfig];
+}
+
+#pragma mark - Config Methods
+- (void)initConfig {
+}
+
+- (void)viewConfig {
+    _locationButton.layer.cornerRadius = _locationButton.frame.size.height/2;
+}
+
+#pragma mark - Event Response
+- (IBAction)locationButtonPressed {
+    if (_finishedBlock) {
+        _finishedBlock();
+        [self showed];
+    }
+    [self hidden];
+}
+
+#pragma mark - Public Methods
+- (void)showGuide:(void(^)(void))finished {
+    _finishedBlock = finished;
+    [self show];
+}
+
+- (void)hidden {
+    __weak __typeof__(self)weakSelf = self;
+    [UIView animateWithDuration:0.8f animations:^{
+        __strong __typeof__(self)strongSelf = weakSelf;
+        strongSelf.alpha = 0.0f;
+    } completion:^(BOOL finished) {
+        __strong __typeof__(self)strongSelf = weakSelf;
+        [strongSelf removeFromSuperview];
+    }];
+}
+
+#pragma mark - Private Methods
+- (void)show {
+    AppDelegate *delegate = [UIApplication sharedApplication].delegate;
+    UIWindow *mainWindow = delegate.window;
+    self.frame = mainWindow.frame;
+    [mainWindow addSubview:self];
+}
+
+- (void)showed {
+    [[NSUserDefaults standardUserDefaults] setValue:@(YES) forKey:kGuideViewShowKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+#pragma mark - UIScrollViewDelegate Methods
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    CGFloat page = scrollView.contentOffset.x/scrollView.frame.size.width;
+    _pageControl.currentPage = page;
+    if (page > 3.2f) {
+        [self locationButtonPressed];
+    }
+}
+
+@end
